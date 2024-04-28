a86c1dd6695e1a000dd5cc99d9ac1f708b34c21d
Asren <gaoxw@feelingtech.net>
2024-04-28 21:12:35 +0800

    fix: stylelint

---
+++ packages/stylelint-config/__test__/rules-validate.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert')
+const stylelint = require('stylelint')
+
+describe('rules-validate.test.js', () => {
+  it('styles lint', () => {
+    const filePath = path.join(__dirname, './fixtures/index.css')
+  })
+})
--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,9 +1,34 @@
-
 module.exports = {
   defaultSeverity: 'warning',
   plugins: ['stylelint-scss'],
   rules: {
+    /**
+     * css 规则配置
+     * https://stylelint.io/user-guide/rules
+     */
     "at-rule-no-unknown": null,
     'scss/at-rule-no-unknown': true,
+    'block-no-empty': null,
+    'color-no-invalid-hex': true,
+    'comment-no-empty': true,
+    'declaration-block-no-duplicate-properties': [
+      true,
+      {
+        ignore: ['consecutive-duplicates-with-different-values']
+      }
+    ],
+    'declaration-block-no-shorthand-property-override': true,
+    'font-family-no-duplicate-names': true,
+    'function-calc-no-unspaced-operator': true,
+    'function-linear-gradient-no-nonstandard-direction': true,
+    'keyframe-declaration-no-important': true,
+    'media-feature-name-no-unknown': true,
+    'no-descending-specificity': null, // @reason 实际有很多这样用的，且多数人熟悉 css 优先级
+    'no-duplicate-at-import-rules': true,
+    'no-duplicate-selectors': true,
+    'no-empty-source': null,
+    'no-extra-semicolons': true,
+    'no-invalid-double-slash-comments': true,
+    'property-no-unknown': true,
   }
 }
