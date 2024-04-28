a81cd6f930c3ff4995dc83cdba59219aab3441ea
Asren <gaoxw@feelingtech.net>
2024-04-28 22:32:14 +0800

    feat: style-jest

--- packages/stylelint-config/__test__/rules-validate.test.js
+++ packages/stylelint-config/__test__/rules-validate.test.js
@@ -2,7 +2,13 @@ const assert = require('assert')
 const stylelint = require('stylelint')
 
 describe('rules-validate.test.js', () => {
-  it('styles lint', () => {
+  it('styles lint', async() => {
     const filePath = path.join(__dirname, './fixtures/index.css')
+
+    const result = await stylelint.lint({
+      configFile: path.join(__dirname, '../index.js'),
+      files: filePaths,
+      fix: false,
+    });
   })
 })
